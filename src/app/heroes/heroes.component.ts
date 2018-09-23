751b4cffa7182f09cc02fc47713c32c21e7d45ed
neilzhu2 <neilzhu@umich.edu>
2018-09-23 16:31:23 -0400

    service&message and image

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero；
+  heroes: Hero[];
 
+  constructor(private heroService: HeroService) {}
 
-  heroes = HEROES;
+  ngOnInit() {
+    this.getHeroes();
+  }
 
-  selectedHero: Hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+                    .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
@@ -29,11 +38,6 @@ export class HeroesComponent implements OnInit {
       buttonElem.innerHTML = "Order Alphabetically";
       heroes.sort((h1, h2) => h1.id > h2.id ? 1:-1)
     }
-  }
-
-  constructor() { }
-
-  ngOnInit() {
   }
 
 }
